068a6194586f4d8e24ade9f818de60eaeeda129f
Drzimi <124393239+Drzimi@users.noreply.github.com>
2023-05-04 20:24:33 -0600

    Add files via upload

---
+++ empleados/src/main/java/com/mycompany/empleados/Empleados.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.empleados;
+
+/**
+ *
+ * @author papic
+ */
+abstract public class Empleados {
+
+    public static void main(String[] args) {
+        
+    }
+}
+class Empleados(Nombre, Apellido , Edad , Salario){
+      private String Nombre;
+      private String Apellido;
+      private double Edad;
+      private double Salario;
+      
+   
+       
+    public Nombre (String Nombre){
+        Nombre = Nombre;
+    }
+    public double getNombre(){
+        return Nombre;
+    }
+    public double setNombre(){
+        return Nombre;
+    }
+    public Apellido (String Apellido){
+        Apellido = Apellido;
+    }
+    public double getApellido(){
+        return Apellido;
+    }
+    public double setApellido(){
+        return Apellido;
+    }
+    public Edad (int Edad){
+        Edad = Edad;
+    }
+    public double get Edad(){
+        return Edad;
+    }
+    public double set Edad(){
+        return Edad;
+    }
+    public Salario (Dobule Salario){
+        Salario = Salario;
+    }
+    public double get SalarioEdad(){
+        return Salario;
+    }
+    public double set SalarioEdad(){
+        return Salario;
+    }
+   }
